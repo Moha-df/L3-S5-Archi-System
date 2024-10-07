435a328e8a462bde9efdcee455ff54cae7328d02
Moha <pro.defranceschi.mohamed@gmail.com>
2024-10-07 18:52:26 +0200

    en cours

--- 1.2-z33-somme/somme.s
+++ 1.2-z33-somme/somme.s
@@ -1,8 +1,39 @@
 //////////////////////////////////////////////////////////////////////////////
 
 // programme "somme" placé à l'adresse 2000
+.addr 2000
+somme:
+        cmp 0, %b
+        jle endSomme //a changer
+        push %a
+        ld [%a], %a
+        push %b
+        add %a, %b
+        pop %a
+        add 1, %a
+        st %b, [%a]
+        pop %b
+        sub 1, %b
+        jmp somme
+endSomme:
+        ld [%a], %a
+        rtn
+        
 
+main_somme:
+        push %b
+        ld 3, %b
+        ld [tab], %a
+        call somme
+        pop %b
         reset
 
 
-main_somme:
+
+tab:
+        // tab_0
+        .word 2
+        // tab_1
+        .word -3
+        // tab_2
+        .word -7
