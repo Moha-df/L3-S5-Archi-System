885d5478d3ac24fa921a4307e26cf9bc2ee64d00
Moha <pro.defranceschi.mohamed@gmail.com>
2024-11-27 10:08:06 +0100

    gg

--- 2.4-thr-dep/serpar.c
+++ 2.4-thr-dep/serpar.c
@@ -42,6 +42,13 @@ void tache(int i)
     printf("%d\n", i);
 }
 
+void *execute_tache(void *arg)
+{
+    int t = *((int *)arg);
+    tache(t);
+    return NULL;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -49,12 +56,50 @@ int main(int argc, char *argv[])
         usage(argv[0]);
 
 
-    pthread_t threads;
+    // Création des threads pour chaque tâche
+    pthread_t threads[11];  // Il y a 11 tâches (1 à 42)
+    int t1 = 1, t21 = 21, t22 = 22, t31 = 31, t32 = 32, t33 = 33, t41 = 41, t42 = 42, t5 = 5, t34 = 34;
+    // Exécution des tâches dans l'ordre des dépendances
 
-    pthread_create(&threads, NULL, tache, (void*)1);
+    // Tâche 1
+    CHK(pthread_create(&threads[0], NULL, execute_tache, (void *)&t1));
 
+    // Attendre que la tâche 1 soit terminée avant de lancer 21 et 22
+    pthread_join(threads[0], NULL);
+    // Tâches 21 et 22 après la tâche 1
+    CHK(pthread_create(&threads[1], NULL, execute_tache, (void *)&t21));
+    CHK(pthread_create(&threads[2], NULL, execute_tache, (void *)&t22));
 
-    pthread_join(threads, NULL);
+    // Attendre que les tâches 21 soit terminées avant de lancer 31, 32, 33
+    pthread_join(threads[1], NULL);
+    // Tâches 31, 32, 33 après 21
+    CHK(pthread_create(&threads[3], NULL, execute_tache, (void *)&t31));
+    CHK(pthread_create(&threads[4], NULL, execute_tache, (void *)&t32));
+    CHK(pthread_create(&threads[5], NULL, execute_tache, (void *)&t33));
+
+    // Attendre que les tâches 22 soit terminées avant de lancer 34
+    pthread_join(threads[2], NULL);
+    CHK(pthread_create(&threads[6], NULL, execute_tache, (void *)&t34));
+
+    // Attendre que 31, 32 et 33 soient terminées avant de lancer 41
+    pthread_join(threads[3], NULL);
+    pthread_join(threads[4], NULL);
+    pthread_join(threads[5], NULL);
+    // Tâches 41 après 31, 32, 33
+    CHK(pthread_create(&threads[7], NULL, execute_tache, (void *)&t41));
+
+    // Attendre que les tâches 34 soit terminées avant de lancer 42
+    pthread_join(threads[6], NULL);
+    CHK(pthread_create(&threads[8], NULL, execute_tache, (void *)&t42));
+
+    // Attendre que les tâches 41 et 42 soient terminées avant de lancer la tâche 5
+    pthread_join(threads[7], NULL);
+    pthread_join(threads[8], NULL);
+    // Tâche 5 après 41 et 42
+    CHK(pthread_create(&threads[9], NULL, execute_tache, (void *)&t5));
+
+    // Attendre que la tâche 5 soit terminée
+    pthread_join(threads[9], NULL);
 
 
     exit(0);
