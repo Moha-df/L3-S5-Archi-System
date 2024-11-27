0f5a481de1943d4e2fb5c26bb56d9cd6a51820bb
Moha <pro.defranceschi.mohamed@gmail.com>
2024-11-27 07:43:30 +0100

    4.2 en cours

--- 4.2-mutex-granularite/ctypes.c
+++ 4.2-mutex-granularite/ctypes.c
@@ -55,12 +55,90 @@ unsigned long int n_punct = 0;
 unsigned long int n_space = 0;
 unsigned long int n_other = 0;
 
+#ifdef UNIQUE
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *analyser_fichier(void *arg)
+{
+    FILE *file = (FILE *)arg;
+    int c;
+
+    // Lire chaque caractère du fichier
+    while ((c = fgetc(file)) != EOF)
+    {
+        // Protéger les accès aux compteurs
+        pthread_mutex_lock(&mutex);
+
+        if (isalnum(c))
+            n_alnum++;
+        else if (ispunct(c))
+            n_punct++;
+        else if (isspace(c))
+            n_space++;
+        else
+            n_other++;
+
+        // Déverrouiller le mutex
+        pthread_mutex_unlock(&mutex);
+    }
+
+    return NULL;
+}
+#endif
+
+
+
+
+
 // Les mutex peuvent aussi être globaux pour simplifier
 
 int main(int argc, char *argv[])
 {
     if (argc == 1)
         usage(argv[0]);
+
+    pthread_t *threads = malloc((argc - 1) * sizeof(pthread_t));
+    if (threads == NULL){
+        raler("malloc");
+    }
+        
+    
+    for (int i = 1; i < argc; i++)
+    {
+        FILE *file = fopen(argv[i], "r");
+        if (file == NULL)
+        {
+            fprintf(stderr, "arg pas un fichier");
+            exit(1);
+            continue;
+        }
+
+        #ifdef UNIQUE
+
+         if (pthread_create(&threads[i - 1], NULL, analyser_fichier, file) != 0)
+        {
+            perror("pthread_create");
+            fclose(file);
+            continue;
+        }
+
+        #endif
+
+    }
+
+
+    #ifdef UNIQUE
+    for (int i = 0; i < argc - 1; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    #endif
+
+    // Afficher les résultats
+    printf("%lu %lu %lu %lu \n", n_alnum, n_punct, n_space, n_other);
+    
+    free(threads);
+
 
     exit(0);
 }
