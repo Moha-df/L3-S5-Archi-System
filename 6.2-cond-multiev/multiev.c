2d4367dc8dcbad55faa9a31b4022ffbf2039c41e
Moha <pro.defranceschi.mohamed@gmail.com>
2024-11-27 12:02:15 +0100

    6.2

--- 6.2-cond-multiev/multiev.c
+++ 6.2-cond-multiev/multiev.c
@@ -36,21 +36,36 @@ noreturn void usage(char *argv0)
     exit(1);
 }
 
+// Structure pour stocker les arguments de chaque thread
+typedef struct {
+    int delai;  // Délai en millisecondes
+    int numero; // Numéro du thread
+    pthread_cond_t *cond;  // Condition pour signaler la fin du délai
+    pthread_mutex_t *mutex; // Mutex pour protéger la condition
+    int can_terminate;
+} thread_arg_t;
+
 void *fonction(void *arg)
 {
-    int moi, delai;
-
     // TODO mon numéro de thread (1..n)
-    moi = 0;
     // TODO l'attente qui m'a été communiquée par le thread principal
-    delai = 0;
+    thread_arg_t *params = (thread_arg_t *)arg;
+    int delai = params->delai;
+    int numero = params->numero;
+    pthread_cond_t *cond = params->cond;
+    pthread_mutex_t *mutex = params->mutex;
 
     usleep(delai * 1000);
-    printf("T %d fin usleep\n", moi);
+    printf("T %d fin usleep\n", numero);
 
     // TODO envoyer l'information au thread principal
+    // Signaler que le thread a terminé son attente
+    pthread_mutex_lock(mutex);
+    params->can_terminate = 1;
+    pthread_cond_signal(cond);
+    pthread_mutex_unlock(mutex);
 
-    printf("T %d terminé\n", moi);
+    printf("T %d terminé\n", numero);
     return NULL;
 }
 
@@ -64,13 +79,57 @@ int main(int argc, char *argv[])
     // TODO tester la validité des arguments, puis créer les threads
     // les tests doivent être effectués avant de commencer les créations
 
+
+    // Nombre de threads à créer (n)
+    int n = argc - 1;
+    int delais[n];
+
+    // Lecture des délais à partir des arguments
+    for (int i = 0; i < n; i++) {
+        delais[i] = atoi(argv[i + 1]);
+        if (delais[i] <= 0) {
+            usage(argv[0]);
+        }
+    }
+
+    // Initialisation du mutex et des conditions
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+    pthread_t threads[n];
+    thread_arg_t args[n];
+
+    // Créer les threads
+    for (int i = 0; i < n; i++) {
+        args[i].delai = delais[i];
+        args[i].numero = i + 1;
+        args[i].cond = &cond;
+        args[i].mutex = &mutex;
+        args[i].can_terminate = 0;
+        TCHK(pthread_create(&threads[i], NULL, fonction, &args[i]));
+    }
+
     // TODO attendre la fin des usleep de tous les threads
     // À chaque fois qu'un thread nous notifie sa fin d'attente,
     // il faut faire :
     //          printf ("P reçu T %d terminé\n", ....) ;
     // (avec le bon numéro de thread)
 
+    // Attente que tous les threads terminent leur délai
+    for (int i = 0; i < n; i++) {
+        pthread_mutex_lock(&mutex);
+        while(!args[i].can_terminate){
+            pthread_cond_wait(&cond, &mutex); // Attendre que le thread i termine son délai
+        }
+        printf("P reçu T %d termine\n", i + 1);
+        pthread_mutex_unlock(&mutex);
+    }
+
     // TODO attendre la terminaison des threads
+    // Attendre la terminaison de tous les threads
+    for (int i = 0; i < n; i++) {
+        TCHK(pthread_join(threads[i], NULL));
+    }
 
     printf("P terminé\n");
 
