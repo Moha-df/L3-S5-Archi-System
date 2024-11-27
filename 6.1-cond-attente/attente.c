38186c918a522f7ca0a7106ca4d0bf5860d5cd7d
Moha <pro.defranceschi.mohamed@gmail.com>
2024-11-27 11:15:03 +0100

    gg

--- 6.1-cond-attente/attente.c
+++ 6.1-cond-attente/attente.c
@@ -40,16 +40,28 @@ struct argument
 {
     int d2;
     // TODO il faut sans doute ajouter d'autres champs à cette structure
+    pthread_cond_t *cond;
+    pthread_mutex_t *mutex;
+    int can_terminate; // indique si T2 peut se terminer
 };
 
 void *fonction(void *arg)
 {
     struct argument *a = arg;
 
+
+
     usleep(a->d2 * 1000);
     printf("T2 fin usleep\n");
 
     // TODO attendre le signal du thread principal
+
+    pthread_mutex_lock(a->mutex);
+        while (!a->can_terminate)
+    {
+        pthread_cond_wait(a->cond, a->mutex);
+    }
+    pthread_mutex_unlock(a->mutex);
 
     printf("T2 terminé\n");
     return NULL;
@@ -60,6 +72,8 @@ int main(int argc, char *argv[])
     int d1, d2;
     pthread_t tid;
     struct argument arg;
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
     setbuf(stdout, NULL); // ne pas bufferiser les affichages
 
@@ -76,13 +90,22 @@ int main(int argc, char *argv[])
 
     arg.d2 = d2;
     // TODO il faut sans doute d'autres initialisations
+    arg.cond = &cond;
+    arg.mutex = &mutex;
+    arg.can_terminate = 0; // indique que T2 ne peux pas se terminer
 
+    
     TCHK(pthread_create(&tid, NULL, fonction, &arg));
 
     usleep(d1 * 1000);
     printf("T1 fin usleep\n");
 
     // TODO indiquer à T2 qu'il peut se terminer
+    pthread_mutex_lock(&mutex);
+    arg.can_terminate = 1;
+    pthread_cond_signal(&cond);  
+    pthread_mutex_unlock(&mutex);
+
 
     printf("T1 attente terminaison T2\n");
     TCHK(pthread_join(tid, NULL));
